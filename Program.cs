0c041ab93dccda27a349b3c3e3ef67b23f948ce7
xToMsIx <168100955+xToMsIx@users.noreply.github.com>
2025-05-26 17:45:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.DAL.EF;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.Services.Configuration.AutoMapperProfiles;
+
+var builder = WebApplication.CreateBuilder(args); // ← najpierw tworzysz buildera!
+
+// Add services to the container.
+
+builder.Services.AddAutoMapper(typeof(MainProfile).Assembly); // AutoMapper - automatycznie wczyta wszystkie profile z projektu
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddUserManager<UserManager<User>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddTransient(typeof(ILogger), typeof(Logger<Program>));
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+app.Run();
